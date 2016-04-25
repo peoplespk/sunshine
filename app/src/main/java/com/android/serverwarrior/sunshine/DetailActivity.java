941a01836d5eb8c52b5c74d1bd696a94ca2e6e2c
prafullakumarsahu <prafullakumarsahu@gmail.com>
2016-04-25 20:36:04 +0530

    Adds setting activity

--- app/src/main/java/com/android/serverwarrior/sunshine/DetailActivity.java
+++ app/src/main/java/com/android/serverwarrior/sunshine/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.android.serverwarrior.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -46,6 +47,7 @@ public class DetailActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if(id == R.id.action_settings){
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
