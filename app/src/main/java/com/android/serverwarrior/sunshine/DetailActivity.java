ffef90cae986b99057c35cc61e37fa1bbdf1e02b
prafullakumarsahu <prafullakumarsahu@gmail.com>
2016-04-26 20:25:41 +0530

    Adds share option

--- app/src/main/java/com/android/serverwarrior/sunshine/DetailActivity.java
+++ app/src/main/java/com/android/serverwarrior/sunshine/DetailActivity.java
@@ -4,21 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 public class DetailActivity extends AppCompatActivity {
-
+    private static final String LOG_TAG = DetailActivity.class.getSimpleName() ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
   /*      if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment, new PlaceholderFragment())
+                    .add(R.id.fragment, new DetailFragment())
                     .commit();
         }*/
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -39,6 +42,15 @@ public class DetailActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         //return super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.detail, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        if(mShareActionProvider != null){
+            mShareActionProvider.setShareIntent(DetailFragment.createShareForecastIntent());;
+        } else{
+            Log.d(LOG_TAG, "Share Action Provider is nul  ?");
+        }
         return true;
     }
 
---
+++ app/src/main/java/com/android/serverwarrior/sunshine/DetailFragment.java
@@ -0,0 +1,45 @@
+package com.android.serverwarrior.sunshine;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DetailFragment extends Fragment {
+
+    private static final String LOG_TAG = DetailFragment.class.getSimpleName() ;
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    private static String mForecastStr;
+
+    public DetailFragment() {
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Intent intent = getActivity().getIntent();
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)){
+            mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            ((TextView) rootView.findViewById(R.id.detail_text)).setText(mForecastStr);
+
+        }
+        return rootView;
+
+    }
+
+     public static Intent createShareForecastIntent(){
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mForecastStr + FORECAST_SHARE_HASHTAG);
+        return shareIntent;
+    }
+}
